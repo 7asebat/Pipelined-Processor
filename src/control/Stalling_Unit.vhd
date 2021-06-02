b69be8f5221dca17394c92fdc352c319363568f0
Youssef Walid <yousefwalid98@hotmail.com>
2021-06-02 23:35:28 +0200

    Fixed bug with stalling unit not_en was not(activate) when it should be activate

--- src/control/Stalling_Unit.vhd
+++ src/control/Stalling_Unit.vhd
@@ -1,28 +1,28 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.numeric_std.all;
-use IEEE.std_logic_unsigned.all;
-use work.Utility_Pack.all;
+LIBRARY IEEE;
+USE IEEE.std_logic_1164.ALL;
+USE IEEE.numeric_std.ALL;
+USE IEEE.std_logic_unsigned.ALL;
+USE work.Utility_Pack.ALL;
 
-entity Stalling_Unit is 
-  port (
-    is_lw : IN std_logic;
-    lw_reset: OUT std_logic;
-    not_en : OUT std_logic;
-    D_RegA_ID : IN std_logic_vector(REG_ADR_WIDTH-1 DOWNTO 0);
-    D_RegB_ID : IN std_logic_vector(REG_ADR_WIDTH-1 DOWNTO 0);
-    EX_RegB_ID : IN std_logic_vector(REG_ADR_WIDTH-1 DOWNTO 0)
+ENTITY Stalling_Unit IS
+  PORT (
+    is_lw : IN STD_LOGIC;
+    lw_reset : OUT STD_LOGIC;
+    not_en : OUT STD_LOGIC;
+    D_RegA_ID : IN STD_LOGIC_VECTOR(REG_ADR_WIDTH - 1 DOWNTO 0);
+    D_RegB_ID : IN STD_LOGIC_VECTOR(REG_ADR_WIDTH - 1 DOWNTO 0);
+    EX_RegB_ID : IN STD_LOGIC_VECTOR(REG_ADR_WIDTH - 1 DOWNTO 0)
   );
 
-end entity Stalling_Unit;
+END ENTITY Stalling_Unit;
 
-architecture main of Stalling_Unit is
-  SIGNAL activate: std_logic;
-begin
+ARCHITECTURE main OF Stalling_Unit IS
+  SIGNAL activate : STD_LOGIC;
+BEGIN
   activate <=
-    '1' WHEN (is_lw = '1') and (EX_RegB_ID = D_RegA_ID or EX_RegB_ID = D_RegB_ID) else
+    '1' WHEN (is_lw = '1') AND (EX_RegB_ID = D_RegA_ID OR EX_RegB_ID = D_RegB_ID) ELSE
     '0';
 
   lw_reset <= activate;
-  not_en <= not(activate);
-end architecture main;
+  not_en <= activate;
+END ARCHITECTURE main;
