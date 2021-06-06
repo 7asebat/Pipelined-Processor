5b5c5eb87d56100145c0f27622e4bb375a2b7a4b
Youssef Walid <yousefwalid98@hotmail.com>
2021-06-06 15:29:25 +0200

    Removed weird syntax from utility

--- assembler/Utility.py
+++ assembler/Utility.py
@@ -1,4 +1,4 @@
-def pad_memory(Memory: dict[int, str]) -> list[str]:
+def pad_memory(Memory):
     padded_memory = [f'{0:016b}'] * (max(Memory.keys()) + 1)
 
     for i in sorted(Memory):
@@ -72,4 +72,3 @@ def build_lookup_table(filePath):
             table[string] = code
 
     return table
-
